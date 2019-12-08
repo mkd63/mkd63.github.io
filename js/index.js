855b7b48b946671ec2501b3ee39367d802d3d141
mkd63 <sharmaaahan15@gmail.com>
2019-12-08 22:11:41 +0530

    added js
    added javascipt

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function reveal1Mes() {
+  document.getElementById("box1_mes").style.display = "block";
+}
+
+function reveal2Mes() {
+  document.getElementById("box2_mes").style.display = "block";
+}
+
+function reveal3Mes() {
+  document.getElementById("box3_mes").style.display = "block";
+}
